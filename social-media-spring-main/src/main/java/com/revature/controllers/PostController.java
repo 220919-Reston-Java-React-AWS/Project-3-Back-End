4a6c87b05ddda9e117fa9848f18bca2fc71ae65e
Josiah S <josiah124@revature.net>
2022-11-11 11:43:25 -0500

    delete comments

--- social-media-spring-main/src/main/java/com/revature/controllers/PostController.java
+++ social-media-spring-main/src/main/java/com/revature/controllers/PostController.java
@@ -5,14 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.revature.annotations.Authorized;
 import com.revature.models.Post;
@@ -48,4 +41,9 @@ public class PostController {
         User user = (User) session.getAttribute("user");
         return ResponseEntity.ok(this.postService.addOrRemoveLike(post, user));
     }
+
+    @Authorized
+    @DeleteMapping("/delete-post")
+    public void deletePost(@RequestBody Post post) {this.postService.deletePost(post);}
+
 }
