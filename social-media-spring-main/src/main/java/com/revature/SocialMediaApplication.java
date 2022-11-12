5502721a6bee3027cca069f530f7219d8f26ec85
Yasin Adam <74yadam@gmail.com>
2022-11-12 09:11:42 -0500

    changed config

--- social-media-spring-main/src/main/java/com/revature/SocialMediaApplication.java
+++ social-media-spring-main/src/main/java/com/revature/SocialMediaApplication.java
@@ -1,7 +1,13 @@
 package com.revature;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SocialMediaApplication {
@@ -9,5 +15,17 @@ public class SocialMediaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SocialMediaApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override 
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE").allowedOrigins("*")
+			.allowedHeaders("*");
+		}
+	};
+	}
+
 
 }
