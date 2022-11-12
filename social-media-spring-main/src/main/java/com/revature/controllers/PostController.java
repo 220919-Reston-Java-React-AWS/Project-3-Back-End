a0fc4ab90f0e8666bd9e7f6d90978325eb829e24
Yasin Adam <74yadam@gmail.com>
2022-11-12 10:13:30 -0500

    changed origins to all

--- social-media-spring-main/src/main/java/com/revature/controllers/PostController.java
+++ social-media-spring-main/src/main/java/com/revature/controllers/PostController.java
@@ -15,7 +15,7 @@ import com.revature.services.PostService;
 
 @RestController
 @RequestMapping("/post")
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@CrossOrigin(origins = "*")
 public class PostController {
 
     private final PostService postService;
