e034ae41ec391774bccffb29183543a80ec79382
Yasin Adam <74yadam@gmail.com>
2022-11-12 09:21:17 -0500

    changed origin to just local host

--- social-media-spring-main/src/main/java/com/revature/SocialMediaApplication.java
+++ social-media-spring-main/src/main/java/com/revature/SocialMediaApplication.java
@@ -21,7 +21,7 @@ public class SocialMediaApplication {
 		return new WebMvcConfigurer() {
 			@Override 
 		public void addCorsMappings(CorsRegistry registry) {
-			registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE").allowedOrigins("*")
+			registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE").allowedOrigins("http://localhost:3000")
 			.allowedHeaders("*");
 		}
 	};
