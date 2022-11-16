dc824ca576e7cb948610dbad333c4414cf6e5afc
Josiah S <josiah124@revature.net>
2022-11-15 20:36:49 -0500

    added delete comments functionality

--- social-media-spring-main/src/main/java/com/revature/services/CommentService.java
+++ social-media-spring-main/src/main/java/com/revature/services/CommentService.java
@@ -3,9 +3,11 @@ package com.revature.services;
 import com.revature.models.Comment;
 import com.revature.repositories.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class CommentService {
     @Autowired
     public CommentRepository commentRepository;
