614e87a12e0a8c72453e3bc4c843a9cd57264605
Yasin Adam <74yadam@gmail.com>
2022-11-29 14:57:48 -0500

    a few unit tests for followers service

--- social-media-spring-main/src/test/java/com/revature/services/FollowersServiceTest.java
+++ social-media-spring-main/src/test/java/com/revature/services/FollowersServiceTest.java
@@ -1,18 +1,21 @@
 package com.revature.services;
 
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.revature.models.Followers;
 import com.revature.models.User;
 import com.revature.repositories.FollowersRepository;
 import com.revature.repositories.UserRepository;
@@ -32,20 +35,74 @@ public class FollowersServiceTest {
     void testNewFollowWithNoFollowing() {
 
         //Arrange or Given
-        User user = User.builder().id(1).build();
-        User user1 = User.builder().id(2).build();
+        User user = new User();
+        user.setId(1);
+        User toFollow = new User();
+        toFollow.setId(2);
 
+        Followers follow = new Followers();
         List<User> expected = new ArrayList<>();
-        expected.add(user1);
+        expected.add(toFollow);
+        follow.setUser(user);
+        follow.setFollowing(expected);
 
         //Act or When
-        when(ur.findById(user1.getId())).thenReturn(Optional.of(user1));
-        when(fs.newFollow(user, user1.getId())).thenReturn(expected);
-        
-        //Assert or Then
-        List<User> actual = fs.newFollow(user, user1.getId());
+        when(ur.findById(toFollow.getId())).thenReturn(Optional.of(toFollow));
+        when(fr.save(follow)).thenReturn(follow);
+        List<User> actual = fs.newFollow(user, toFollow.getId());
 
-        Assertions.assertEquals(expected, actual);
+        //Assert or Then
+        Assertions.assertThat(expected).isEqualTo(actual);
+    }
+
+    @Test
+    void testNewFollowWithFollowing() {
+        //Arrange or Given
+        User user = new User();
+        user.setId(1);
+        User toFollow = new User();
+        toFollow.setId(2);
+
+        Followers follow = new Followers();
+        List<User> expected = new ArrayList<>();
+        expected.add(toFollow);
+        follow.setUser(user);
+        follow.setFollowing(expected);
+
+
+         //Act or When
+         when(ur.findById(toFollow.getId())).thenReturn(Optional.of(toFollow));
+         when(fr.save(follow)).thenReturn(follow);
+        List<User> actual = fs.newFollow(user, toFollow.getId());
+
+         //Assert or Then
+         Assertions.assertThat(actual).contains(toFollow);
+    }
+
+    @Test
+    void testNewFollowUnfollow() {
+        //Arrange or Given
+        User user = new User();
+        user.setId(1);
+        User toFollow = new User();
+        toFollow.setId(2);
+
+        Followers follow = new Followers();
+        List<User> expected = new ArrayList<>();
+        expected.add(toFollow);
+        follow.setUser(user);
+        follow.setFollowing(expected);
+
+
+         //Act or When
+         when(ur.findById(toFollow.getId())).thenReturn(Optional.of(toFollow));
+         when(fr.existsByUser(user)).thenReturn(true);
+         when(fr.findByUser(user)).thenReturn(follow);
+         when(fr.save(follow)).thenReturn(follow);
+         List<User> actual = fs.newFollow(user, toFollow.getId());
+
+         //Assert or Then
+         Assertions.assertThat(actual).isEmpty();
     }
 
 }
